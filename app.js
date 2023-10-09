d688a6a066556451fd46da18e43118c077703ceb
masa-massara <masaya.yoshikawa.31@icloud.com>
2023-10-10 04:07:07 +0900

    「止めて」を表示できるように

---
+++ app.js
@@ -0,0 +1,14 @@
+const container = document.querySelector(".container");
+const text = document.getElementById("text");
+
+let totalTime = 7500;
+let breathTime = (totalTime / 5) * 2;
+let holdTime = totalTime / 5;
+
+meditateAnimation();
+
+function meditateAnimation() {
+  setTimeout(() => {
+    text.innerHTML = "止めて"
+  },breathTime);
+}
